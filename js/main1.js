2a55fb9e9411b392bdc73d9f3b8794b9f1707c9a
lalogd <102998680+lalogd@users.noreply.github.com>
2026-01-15 20:57:33 -0600

    Update main1.js

--- js/main1.js
+++ js/main1.js
@@ -5,7 +5,7 @@ let resultStorage = "results";
 let aliasStorage = "alias";
 
 let price = 0;  //replace *0
-let tel = "525647806645";
+let tel = "525556209648";
 let nPartidos = 10;
 let partidoSup = false;
 
@@ -375,6 +375,7 @@ function send(){				//Envia la quiniela al whatsapp
 
 
 window.addEventListener("load",start,false);
+
 
 
 
